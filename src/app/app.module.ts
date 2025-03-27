917d2022da5e15f54bc269f677dc7c805cee83c1
moria1234 <moria.muharer@gmail.com>
2025-03-27 14:33:40 +0200

    סידור כפילויות בקובץ app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // חובה בשביל ngModel
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-=======
-import { AppComponent } from './app.component';
-import { ProductQuestionsComponent } from './product-questions/product-questions.component';
-import { ProductService } from './services/product.service';
-import { QuizComponent } from './quiz/quiz.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AppRoutingModule } from './app-routing.module'; // Import routing module
-import { ResultsComponent } from './results/results.component';
->>>>>>> 6a6ae677ef94838faa2388bdca4d7bfabefcaf25
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -27,7 +16,6 @@ import { ProductService } from './services/product.service';
     AppComponent,
     HomeComponent,
     QuizComponent,
-<<<<<<< HEAD
     ProductQuestionsComponent,
     ResultsComponent,
     ProfileComponent, // להוסיף אותו כאן
@@ -42,17 +30,6 @@ import { ProductService } from './services/product.service';
       { path: 'results', component: ResultsComponent },
       { path: 'profile', component: ProfileComponent },
     ]),
-=======
-    HomeComponent, 
-    ProfileComponent,
-    ResultsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-    AppRoutingModule,
->>>>>>> 6a6ae677ef94838faa2388bdca4d7bfabefcaf25
   ],
   providers: [ProductService],
   bootstrap: [AppComponent],
